d7f0c51f09054910b5a95c2ac11d9fb7da5ac1d5
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-09-27 09:01:38 -0700

    Images scraped; showfetcher updated

--- mattbakerscraper.py
+++ mattbakerscraper.py
@@ -1,41 +1,37 @@
+import os
 import requests
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
-# Function to get all the links from a page
+root_url = "https://www.mattbakercomedy.com/"
+category_ = ""
+categories = ['jugglers', 'magicians', 'comedians', 'variety']
+url = f"https://www.mattbakercomedy.com/{category_}.html"
+
 def get_all_links(url):
-    # Send a GET request to the website
+    print(url)
     response = requests.get(url)
-
-    # If the response status code is 200 (OK)
     if response.status_code == 200:
-        # Parse the page content with BeautifulSoup
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Find all the <a> tags (which define hyperlinks)
         links = soup.find_all('a')
-
-        # Extract the 'href' attribute from each link
         link_sources = [link.get('href') for link in links if link.get('href')]
-
         return link_sources
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
         return []
 
-# Example usage
-root_url = "https://www.mattbakercomedy.com/"
-url = "https://www.mattbakercomedy.com/jugglers.html"
-all_links = get_all_links(url)
-
-# Function to visit all URLs and extract contents of <div> with id="individual"
-def extract_individual_content(input_list):
-
+def extract_individual_content(input_list, category):
     all_content = []
+    artist = {"name": "",
+              "byline": "",
+              "category": "",
+              "description": "",
+              "image": "",
+              "alt-text": "",}
 
     # Loop over all the links
     for input in input_list:
         new_url = root_url + input
-        # print(new_url + "1")
         try:
             # Fetch the content of each URL
             response = requests.get(str(new_url))
@@ -44,18 +40,27 @@ def extract_individual_content(input_list):
             if response.status_code == 200:
                 # Parse the HTML content of the page
                 soup = BeautifulSoup(response.content, 'html.parser')
-                print(soup)
                 # Find the <div> element with id="individual"
                 individual_div = soup.find('div', class_='individual')
-                print(new_url)
-                print(individual_div)
 
                 if individual_div:
+                    
                     # Remove <img> tags within the div
                     for img_tag in individual_div.find_all('img'):
+                        img_src = img_tag['src']
+                        parsed_url = urlparse(img_src)
+                        artist["name"] = img_tag['title']
+                        artist["image"] = os.path.basename(parsed_url.path)
+                        artist["category"] = category
                         img_tag.decompose()  # Removes the <img> tag
+                        for br in individual_div.find_all(text=True):
+                            if '\n' in br:
+                                br.replace_with(br.replace('\n', '<br>'))
+                        artist["description"] = str(individual_div)
 
                     # Append the remaining HTML content of the div to the list
+                    with open("output.csv", "a") as file:
+                        file.write(f"{artist['name']},{artist['category']},{artist['image']},\"{artist['description']}\"\n")
                     all_content.append(str(individual_div))
 
             else:
@@ -66,12 +71,13 @@ def extract_individual_content(input_list):
 
     return all_content
 
-get_all_links(url)
-new_all_links = list(set([link for link in all_links if "/" in link]))
-print (new_all_links)
+for category in categories:
+    all_links = get_all_links(root_url + category + ".html")
+    artist_pages = list(set([link for link in all_links if "/" in link]))
+    content_from_individuals = extract_individual_content(artist_pages, category)
 
-content_from_individual = extract_individual_content(new_all_links)
+# print (content_from_individuals)
 
-# Print or process the extracted content
-for content in content_from_individual:
-    print(content)
+# # Print or process the extracted content
+# for content in content_from_individuals:
+#     print(content)
--- showfetcher.js
+++ showfetcher.js
@@ -1,109 +1,67 @@
-const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsA8WweuIqAmaLf9oY38_ezsd_LCOrgTrGEh7eXQBy91d2xwD-NqBlFdc6GvBkay8ZgRCWbwzjHAZM/pub?output=csv';
+// const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsA8WweuIqAmaLf9oY38_ezsd_LCOrgTrGEh7eXQBy91d2xwD-NqBlFdc6GvBkay8ZgRCWbwzjHAZM/pub?output=csv';
+const url = 'https://docs.google.com/spreadsheets/d/1tFTQRY0HDTZCMGWQy0helNDCgRiW73HlDQtfo9FKldg/pub?output=csv';
 let rows = [];
 let counter = 0;
 let lazy = false;
-let allShows = [];
-let showSection = '';
+let allArtists = [];
+let artistTemplate = '';
 
-function addShow(show, lazy) {
-    let showName = show[0];
-    let showWeekday = show[1];
-    let showTime = show[2];
-    let showDate = show[3];
-    let showDesc = show[4];
-    let showImage = show[5];
-    let showAltText = show[6];
+function addArtist(artist) {
+    let artistName = artist[0];
+    let artistByline = artist[1];
+    let artistCategory = artist[2];
+    let artistDesc = artist[3];
+    let artistImage = artist[5];
+    let artistAltText = artist[6];
     let lazyTag = '';
     if (lazy) {
         lazyTag = 'loading="lazy"';
     }
-    let showTemplate = `
-        <figure class="show">
-            <img class="show-pic" src="/images/music/${showImage}" ${lazyTag} alt="${showAltText}" />
-            <figcaption class="show-info">
+    let artistTemplate = `
+        <figure class="artist">
+            <img class="artist-pic" src="/images/music/${artistImage}" alt="${artistAltText}" />
+            <figcaption class="artist-info">
                 <section class="music-bio">
-                    <h2>${showName}</h2>
-                    <h6>${showDesc}</h6>
+                    <h2>${artistName}</h2>
+                    <h6>${artistDesc}</h6>
                 </section>
-                <section class="showtime">
-                    <h6>${showWeekday}, ${showDate}<br>${showTime}</h6>
+                <section class="artisttime">
+                    <h6>${artistWeekday}, ${artistDate}<br>${artistTime}</h6>
                 </section>
             </figcaption>
         </figure>
     `;
-    allShows.push(showTemplate);
+    allArtists.push(artistTemplate);
 }
 
-function fetchShowSheet(url) {
+function fetchArtistSheet(url) {
     fetch(url)
     .then(response => response.text())
     .then(data => {
         Papa.parse(data, {
             complete: function(results) {
                 rows = results.data;
-                for (let show of rows) {
+                for (let artist of rows) {
                     if (counter > 3 && !lazy) {
                         lazy = true;
                     }
-                    if (show[7] && show[7].trim() === 'TRUE') {
-                        addShow(show, lazy);
+                    if (artist[7] && artist[7].trim() === 'TRUE') {
+                        addArtist(artist, lazy);
                     }
                     counter++;
                 }
 
-                showSection = `
+                artistSection = `
                 <h1>MUSIC</h1>
-                ${allShows.join('\n')}
-                    <!-- <figure class="soon">
-                        <br>
-                        <h3>A L S O</h3><br>
-                        <h4>The White Ducks</h4>
-                        <h5>August 20</h5><br>
-                    </figure> -->
+                ${allArtists.join('\n')}
 
-                    <figure class="previously">
-                        <h3>P R E V I O U S L Y</h3>
-                        <h5>
-                        The White Ducks<br>
-                        Raggedly Adler<br>
-                        Brooke Bates<br>
-                        Tad Kroening<br>
-                        Ludlings<br>
-                        Towse<br>
-                        Channing Waage<br>
-                        Matney Cook<br>
-                        Ivan Sandomire<br>
-                        Roger Thorne<br>
-                        Shannon Patiño<br>
-                        Louis Ledford<br>
-                        Norah McLaughlin<br>
-                        Cullen Gray<br>
-                        Morgan McHugh<br>
-                        Pete Irving & Mickey Stylin<br>
-                        Tenderpile's Smile Aisle</br>
-                        Lucas Warford<br>
-                        Free Harmony<br>
-                        Sarah Goodin<br>
-                        Sam Chue<br>
-                        Chuck and Carr<br>
-                        The House Wrens<br>
-                        Octavia McAloon<br>
-                        David Donohue<br>
-                        Ron Hardesty<br>
-                        Paige Woods<br>
-                        The Moving Hats<br>
-                        Blue Collar Grass Band<br>
-                        Emily Freudenberger<br>
-                        Badd Dogg Howl-O-Ween<br>
-                        Russell Thompson</h5>
-                    </figure>
                 `;
 
-                document.getElementById('music-wrapper').innerHTML = showSection;
+                document.getElementById('music-wrapper').innerHTML = artistSection;
             }
         });
     })
     .catch(error => console.error('Error fetching the Google Sheet:', error));
 }
 
-fetchShowSheet(url);
+fetchArtistSheet(url);
